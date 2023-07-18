4cece3a17e64b1d98aafbe570377b5c84cad3a2a
YusuCoder <ryusupov788@gmail.com>
2023-07-18 21:21:13 +0500

    added new project

---
+++ app/src/main/java/com/example/tictacox/ResultDialog.java
@@ -0,0 +1,45 @@
+package com.example.tictacox;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.tictacox.MainActivity;
+import com.example.tictacox.R;
+
+public class ResultDialog extends Dialog {
+
+    private final String message;
+    private final MainActivity mainActivity;
+
+    public ResultDialog(@NonNull Context context, String message, MainActivity mainActivity) {
+        super(context);
+        this.message = message;
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_dialog);
+
+        TextView messageText = findViewById(R.id.messageText);
+        Button startAgainButton = findViewById(R.id.startAgainButton);
+
+        messageText.setText(message);
+
+        startAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.restartMatch();
+                dismiss();
+            }
+        });
+    }
+}
